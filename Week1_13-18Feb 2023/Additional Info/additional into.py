1147f482b9b65615a97fa68a63e7e48693a892e7
Grace Gitome <gitomegracie@gmail.com>
2023-02-16 02:24:27 +0300

    eda

--- Week1_13-18Feb 2023/Additional Info/additional into.py
+++ Week1_13-18Feb 2023/Additional Info/additional into.py
@@ -1,2 +1,6 @@
 # This file contains infor and links that are useful
 '''Color Codes : https://matplotlib.org/examples/color/colormaps_reference.html'''
+fig = px.bar(df_2018['City'].value_counts(),
+             title='Climate Region Value counts').update_layout(xaxis_title='Climate Region',yaxis_title='count')
+fig.update_traces(marker_color='violet')
+fig.show()

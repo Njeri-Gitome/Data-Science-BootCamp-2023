1ec92e224eba94aa5ed3e4b4099e700d8e8546af
Grace Gitome <gitomegracie@gmail.com>
2023-02-28 15:16:27 +0300

    sql

--- Week2_20-25Feb 2023/Hands On Practice/SQL HackerRank_ AdvancedJoins.sql
+++ Week2_20-25Feb 2023/Hands On Practice/SQL HackerRank_ AdvancedJoins.sql
@@ -1,3 +1,74 @@
+/* Julia conducted a days of learning SQL contest. The start date of the contest was March 01, 2016 and 
+the end date was March 15, 2016.
+Write a query to print total number of unique hackers who made at least 1 submission
+each day (starting on the first day of the contest), and find the hacker_id and name of the
+hacker who made maximum number of submissions each day. If more than one such hacker has a maximum number
+of submissions, print the lowest hacker_id. The query should print this  information for each day of the 
+contest, sorted by the date.*/
+
+CREATE DATABASE HackerRank;
+
+USE HackerRank;
+
+/* Creating Tables*/
+
+CREATE TABLE Hackers (
+    hacker_id INT PRIMARY KEY,
+    name VARCHAR (255)
+);
+
+CREATE TABLE Submissions(
+    submission_date DATE,
+    submission_id INT PRIMARY KEY,
+    hacker_id INT,
+    score INT,
+    FOREIGN KEY (hacker_id) REFERENCES Hackers(hacker_id)
+
+
+);
+
+/*Inserting Sample records into the tables*/
+INSERT INTO Hackers (hacker_id, name )
+VALUES
+(15758, 'Rose'),
+(20703, 'Angela'),
+(36396, 'Frank'),
+(38289, 'Patrick'),
+(44065, 'Lisa'),
+(53473, 'Kimberly'),
+(62529, 'Bonnie'),
+(79722, 'Michael');
+
+
+INSERT INTO Submissions (submission_date,submission_id, hacker_id, score )
+VALUES
+('2016-03-01', 8494, 20703, 0),
+('2016-03-01', 22403, 53473, 15),
+('2016-03-01', 23965, 79722, 60);
+
+/*Insert additional data*/
+
+INSERT INTO Submissions (submission_date,submission_id, hacker_id, score )
+VALUES
+('2016-03-01', 30173, 36396, 70),
+('2016-03-02', 34928, 20703, 0),
+('2016-03-02', 38740, 15758, 60),
+('2016-03-02', 42769, 79722, 25),
+('2016-03-02', 44364, 79722, 60),
+('2016-03-03', 45440, 20703, 0),
+('2016-03-03', 49050, 36396, 70),
+('2016-03-03', 50273, 79722, 5),
+('2016-03-04', 50344, 20703, 0),
+('2016-03-04', 51360, 44065, 90),
+('2016-03-04', 54404, 53473, 65),
+('2016-03-04', 61533, 79722, 45),
+('2016-03-05', 72852, 20703, 0),
+('2016-03-05', 74546, 38289, 0),
+('2016-03-05', 76487, 62529, 0),
+('2016-03-05', 82439, 36396, 10),
+('2016-03-05', 90006, 36396, 40),
+('2016-03-06', 90404, 20703, 0);
+
 SELECT submission_date,
 /*Scalar placement*/  
                  /*Subquery 2 to obtain unique hackers in a day */
